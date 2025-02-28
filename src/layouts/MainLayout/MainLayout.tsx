7ec13bcecd7e1b582e4432050acfb1fbae4c38b1
BOOMHUNK <51675127+BOOMHUNK@users.noreply.github.com>
2025-02-28 11:58:02 +0330

    chore: cleanup &restructure layout and main page

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -6,6 +6,14 @@ type MainLayoutProps = {
 
 export default function MainLayout({ children }: MainLayoutProps) {
   return <div className="main-layout">
+    <div className="papernoise"></div>
+    <h1 className="logo">NA</h1>
+    <span className="title">News Aggregator</span>
+
     {children}
+
+    <div className="footnotes">
+      <q>This is a case study assignment for joining innoscripta done by Shayan Valiyari</q>
+    </div>
   </div>;
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,13 +7,10 @@ export default function Home() {
 
     return (
         <div className="home">
-            <Filters />
-            {/* News Article Posts */}
-            <NewsFeed />
 
-            <div className="footnotes">
-                <span>This is a case study assignment done for joining innoscripta by Shayan Valiyari</span>
-            </div>
+            <Filters />
+            {/* <NewsFeed /> */}
+
         </div>
     );
 }
